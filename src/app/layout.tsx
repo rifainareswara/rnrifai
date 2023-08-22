9b6498115c22d464fe3ab28033a3c541cfcfcf4e
RIZQI NUR RIFA'I <rifai.rizqi3@gmail.com>
2023-08-22 13:41:30 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
         <Navigation />
         {children}
       </body>
+      <head>
+        <link rel="icon" href="/favicon.jpg" sizes="any" />
+      </head>
     </html>
   );
 }
